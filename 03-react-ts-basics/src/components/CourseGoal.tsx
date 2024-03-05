4bfb05814ae50499c32a4b41289b33600a2d723d
Kiran Anand <liteninkiran@hotmail.com>
2024-03-05 19:45:06 +0000

    029 - Building a First Component & Facing a Missing Type

---
+++ 03-react-ts-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,12 @@
+export default function CourseGoal(props: any) {
+
+    return (
+        <article>
+            <div>
+                <h2>Title</h2>
+                <p>Description</p>
+            </div>
+            <button>Delete</button>
+        </article>
+    );
+}
