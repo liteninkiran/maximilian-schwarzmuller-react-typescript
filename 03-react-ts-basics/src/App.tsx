ed9908506634fa88330e0a70b6a5059349add6bf
Kiran Anand <liteninkiran@hotmail.com>
2024-03-06 16:52:21 +0000

    037 - Working with State & Outputting State-based Values

--- 03-react-ts-basics/src/App.tsx
+++ 03-react-ts-basics/src/App.tsx
@@ -13,21 +13,58 @@ export default function App() {
     const [goals, setGoals] = useState<CourseGoal[]>([]);
 
     function handleAddGoal(): void {
+        const newGoal: CourseGoal = {
+            id: Math.random(),
+            title: 'Learn React & TS',
+            description: 'Learn it in depth',
+        }
+        setGoals(prevGoals => [...prevGoals, newGoal]);
         console.log(goals);
-        setGoals([]);
     }
 
-    return (
-        <main>
+    function headerElement() : JSX.Element {
+        return (
             <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
                 <h1>Your Course Goals</h1>
             </Header>
+        );
+    }
+
+    function buttonElement() : JSX.Element {
+        return (
             <button onClick={ handleAddGoal }>
                 Add Goal
             </button>
-            <CourseGoal title="Learn React + TS">
-                <p>Learn it from the ground up</p>
-            </CourseGoal>
-        </main>
-    );
+        );
+    }
+
+    function goalListElement(): JSX.Element {
+        return (
+            <ul>
+                { goals.map(goalListItemElement) }
+            </ul>
+        );
+    }
+
+    function goalListItemElement(goal: CourseGoal): JSX.Element {
+        return (
+            <li key={ goal.id }>
+                <CourseGoal title={ goal.title }>
+                    <p>{ goal.description }</p>
+                </CourseGoal>
+            </li>
+        );
+    }
+
+    function mainElement(): JSX.Element {
+        return (
+            <main>
+                { headerElement() }
+                { buttonElement() }
+                { goalListElement() }
+            </main>
+        );
+    }
+
+    return mainElement();
 }
