eb160f2ebfb8e179a076de18f0b0e9e9cb466d86
Kiran Anand <liteninkiran@hotmail.com>
2024-03-08 11:20:58 +0000

    064 - The Starting Project

--- 04-advanced-components/src/App.tsx
+++ 04-advanced-components/src/App.tsx
@@ -1,32 +1,16 @@
-import Input from './components/Input';
-import Form, { type TFormHandle } from './components/Form';
-import Button from './components/Button';
-import { useRef } from 'react';
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
-    const customForm = useRef<TFormHandle>(null);
-
-    function handleSave(data: unknown) {
-        if (!(data)                         ||
-            !(typeof data === 'object')     ||
-            !(data.hasOwnProperty('name'))  ||
-            !(data.hasOwnProperty('age'))
-        ) {
-            return;
-        }
-        customForm.current?.clear();
-    }
-
     return (
-        <main>
-            <Form onSave={handleSave} ref={customForm}>
-                <Input type='text' label='Name' id='name' />
-                <Input type='number' label='Age' id='age' />
-                <p>
-                    <Button>Save</Button>
-                </p>
-            </Form>
-        </main>
+        <>
+            <Header />
+            <main>
+                <AddTimer />
+                <Timers />
+            </main>
+        </>
     );
 }
 
