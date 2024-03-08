dee5890f38174399250004f24fc53295035ce1f6
Kiran Anand <liteninkiran@hotmail.com>
2024-03-08 11:06:37 +0000

    060 - Exposing Component APIs with useImperativeHandle (with TypeScript)

--- 04-advanced-components/src/App.tsx
+++ 04-advanced-components/src/App.tsx
@@ -1,6 +1,7 @@
 import Input from './components/Input';
-import Form from './components/Form';
+import Form, { type TFormHandle } from './components/Form';
 import Button from './components/Button';
+import { useRef } from 'react';
 
 type TFormData = {
     name: string;
@@ -8,14 +9,17 @@ type TFormData = {
 }
 
 function App() {
+    const customForm = useRef<TFormHandle>(null);
+
     function handleSave(data: unknown) {
         const extractedData = data as TFormData;
         console.log(extractedData);
+        customForm.current?.clear();
     }
 
     return (
         <main>
-            <Form onSave={handleSave}>
+            <Form onSave={handleSave} ref={customForm}>
                 <Input type='text' label='Name' id='name' />
                 <Input type='number' label='Age' id='age' />
                 <p>
--- 04-advanced-components/src/components/Form.tsx
+++ 04-advanced-components/src/components/Form.tsx
@@ -1,10 +1,15 @@
-import { FormEvent, type ComponentPropsWithoutRef } from 'react';
+import { FormEvent, useRef, useImperativeHandle, forwardRef } from 'react';
+import { type ComponentPropsWithoutRef } from 'react';
 
 type TProps = ComponentPropsWithoutRef<'form'> & {
     onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...otherProps }: TProps) {
+const Form = forwardRef<TFormHandle, TProps>(function Form({ onSave, children, ...otherProps }, ref) {
+    const form = useRef<HTMLFormElement>(null);
+    const formHandle: TFormHandle = { clear(): void { form.current?.reset(); }}
+    useImperativeHandle(ref, () => formHandle);
+
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
@@ -13,8 +18,13 @@ export default function Form({ onSave, children, ...otherProps }: TProps) {
     }
 
     return (
-        <form onSubmit={handleSubmit} {...otherProps}>
+        <form onSubmit={handleSubmit} {...otherProps} ref={form}>
             {children}
         </form>
     );
+});
+
+export default Form;
+export type TFormHandle = {
+    clear: () => void;
 }
