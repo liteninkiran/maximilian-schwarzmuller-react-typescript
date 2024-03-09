becb8d51532634c51a4dbdd53ba30c7bd859240d
Kiran Anand <liteninkiran@hotmail.com>
2024-03-09 14:09:54 +0000

    079 - Onwards to Data Fetching!

--- 04-advanced-components/src/App.tsx
+++ 04-advanced-components/src/App.tsx
@@ -1,17 +1,8 @@
-import AddTimer from './components/AddTimer.tsx';
-import Header from './components/Header.tsx';
-import Timers from './components/Timers.tsx';
-import TimersContextProvider from './store/timers-context.tsx';
-
 function App() {
     return (
-        <TimersContextProvider>
-            <Header />
-            <main>
-                <AddTimer />
-                <Timers />
-            </main>
-        </TimersContextProvider>
+        <h1>
+            Data Fetching!
+        </h1>
     );
 }
 
