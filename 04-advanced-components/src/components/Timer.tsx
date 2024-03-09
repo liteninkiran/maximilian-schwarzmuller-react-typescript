4af5e10f2ae8a71c811125381d0f1729b783a3e9
Kiran Anand <liteninkiran@hotmail.com>
2024-03-09 13:32:29 +0000

    075 - Using useEffect() with TypeScript

--- 04-advanced-components/src/components/Timer.tsx
+++ 04-advanced-components/src/components/Timer.tsx
@@ -1,22 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Timer as TProps } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 
 export default function Timer({ name, duration }: TProps) {
-
     const durationMs = duration * 1000;
     const [remainingTime, setRemainingTime] = useState(durationMs);
-    const callback = () => {
-        setRemainingTime(prevTime => prevTime - 50);
-    }
-
-    setInterval(callback, 50);
+    const callback = (): void => setRemainingTime(prevTime => prevTime - 50);
+    useEffect((): void => { setInterval(callback, 50) }, []);
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     return (
         <Container as="article">
             <h2>{name}</h2>
             <p><progress max={durationMs} value={remainingTime}/></p>
-            <p>{}</p>
+            <p>{formattedRemainingTime}</p>
         </Container>
     );
 }
