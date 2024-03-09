905e022d82ec488ed87883a18a203fb8316e4f38
Kiran Anand <liteninkiran@hotmail.com>
2024-03-09 14:00:17 +0000

    077 - useEffect() & Its Dependencies

--- 04-advanced-components/src/components/Header.tsx
+++ 04-advanced-components/src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     return (
         <header>
             <h1>ReactTimer</h1>
-            <Button onClick={timersCtx.isRunning ? timersCtx.stopTimers : timersCtx.startTimers}>{ timersCtx.isRunning ? 'Start' : 'Stop'} Timers</Button>
+            <Button onClick={timersCtx.isRunning ? timersCtx.stopTimers : timersCtx.startTimers}>{ timersCtx.isRunning ? 'Stop' : 'Start'} Timers</Button>
         </header>
     );
 }
--- 04-advanced-components/src/components/Timer.tsx
+++ 04-advanced-components/src/components/Timer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Timer as TProps } from '../store/timers-context.tsx';
+import { Timer as TProps, useTimersContext } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 
 export default function Timer({ name, duration }: TProps) {
@@ -7,15 +7,21 @@ export default function Timer({ name, duration }: TProps) {
     const durationMs = duration * 1000;
     const [remainingTime, setRemainingTime] = useState(durationMs);
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
- 
+
+    const { isRunning } = useTimersContext();
     const intervalCallback = () => setRemainingTime(prevTime => prevTime - 50);
     const effectCallback = () => {
-        const timer = setInterval(intervalCallback, 50);
-        interval.current = timer;
+        let timer: number;
+        if (isRunning) {
+            timer = setInterval(intervalCallback, 50);
+            interval.current = timer;
+        } else if (interval.current) {
+            clearInterval(interval.current);
+        }
         return () => clearInterval(timer);
     }
  
-    useEffect(() => effectCallback(), []);
+    useEffect(() => effectCallback(), [isRunning]);
  
     if (remainingTime <= 0 && interval.current) {
         clearInterval(interval.current);
