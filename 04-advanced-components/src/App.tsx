04b7ed7f842825a1ec3a93e8abe15cc181ca7ece
Kiran Anand <liteninkiran@hotmail.com>
2024-03-10 15:43:10 +0000

    084 - Handling Loading & Error States

--- 04-advanced-components/src/App.tsx
+++ 04-advanced-components/src/App.tsx
@@ -3,43 +3,54 @@ import { type ReactNode } from 'react';
 import { get } from './util/http';
 import BlogPosts, { BlogPost } from './components/BlogPosts';
 import fetchingImg from './assets/data-fetching.png';
-import { z } from 'zod';
+import ErrorMessage from './components/ErrorMessage';
 
-const rawDataBlogPostSchema = z.object({
-    id: z.number(),
-    userId: z.number(),
-    title: z.string(),
-    body: z.string(),
-});
-const expectedResponseDataSchema = z.array(rawDataBlogPostSchema);
+type RawDataBlogPosts = {
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
+}
 
 function App() {
     const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+    const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState<string>();
+
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
+
             try {
-                const data = await get('https://jsonplaceholder.typicode.com/posts', z.array(rawDataBlogPostSchema));
-                const parsedData = expectedResponseDataSchema.parse(data);
-                const blogPosts: BlogPost[] = parsedData.map((rawPost) => ({
-                    id: rawPost.id,
-                    title: rawPost.title,
-                    text: rawPost.body,
+                const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPosts[];
+                const blogPosts: BlogPost[] = data.map(post => ({
+                    id: post.id,
+                    title: post.title,
+                    text: post.body,
                 }));
                 setFetchedPosts(blogPosts);
             } catch (error) {
                 if (error instanceof Error) {
-                    console.log(error.message);
+                    setError((error).message);
                 }
             }
+            setIsFetching(false);
         }
-
         fetchPosts();
     }, []);
 
     let content: ReactNode;
 
+    if (error) {
+        content = (<ErrorMessage text={error} />);
+    }
+
     if (fetchedPosts) {
         content = (<BlogPosts posts={fetchedPosts} />);
+    }
+
+    if (isFetching) {
+        content = (<p id='loading-fallback'>Fetching Posts...</p>);
     }
 
     return (
--- 04-advanced-components/src/util/http.ts
+++ 04-advanced-components/src/util/http.ts
@@ -1,17 +1,10 @@
-import { z } from 'zod';
-
-export async function get<T>(url: string, zodSchema: z.ZodType<T>) {
+export async function get(url: string) {
     const response = await fetch(url);
 
     if (!response.ok) {
         throw new Error('Failed to fetch data.');
     }
 
-    const data = (await response.json()) as unknown;
-
-    try {
-        return zodSchema.parse(data);
-    } catch (error) {
-        throw new Error('Invalid data received from server.');
-    }
+    const data = await response.json() as unknown;
+    return data;
 }
